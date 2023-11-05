4ceebda9330b76d2700375aa7c28f11e0f0da493
linjintin <145752150+linjintin@users.noreply.github.com>
2023-11-05 11:28:02 +0800

    Add files via upload

---
+++ p18.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main(void)
+{
+	char string1[60] = "Welcome";
+	char string2[60];
+
+	int len;
+
+	strcpy(string2, string1);
+	printf("string2 = %s\n", string2);
+
+	len = strlen(string2);
+	printf("字串長度為%d\n", len);//不會算'\0'
+
+	system("pause");
+	return 0;
+}
